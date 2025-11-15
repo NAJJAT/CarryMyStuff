59eb8299ff9cdb93608dd94683fd1bdfb4cc002c
AMMAR NAJJAR <72874797+NAJJAT@users.noreply.github.com>
2025-11-15 00:21:41 -1100

    first comment

---
+++ src/test/java/be/ehb/carrymystuff/CarryMyStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.carrymystuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryMyStuffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
