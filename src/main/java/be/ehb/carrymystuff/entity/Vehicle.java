386158445c7b9f3dd51a628fa23984ba7f705f4e
AMMAR NAJJAR <72874797+NAJJAT@users.noreply.github.com>
2025-11-15 00:36:22 -1100

    adding Entity

---
+++ src/main/java/be/ehb/carrymystuff/entity/Vehicle.java
@@ -0,0 +1,13 @@
+package be.ehb.carrymystuff.entity;
+
+public class Vehicle {
+
+
+    private Long id;
+
+    private String ownerName;
+    private String type;
+    private double capacity; // in cubic meters
+    private String address;
+    private boolean available;
+}
