7123dff5e8e445dee24ec027215918eed5ad759b
AMMAR NAJJAR <72874797+NAJJAT@users.noreply.github.com>
2025-11-20 04:16:32 -1100

    fixing CONTROLLER

--- src/main/java/be/ehb/carrymystuff/controller/UserController.java
+++ src/main/java/be/ehb/carrymystuff/controller/UserController.java
@@ -59,4 +59,6 @@ public class UserController {
     public List<Booking> myBookings(@AuthenticationPrincipal UserDetails principal) {
         return bookingService.getBookingsForCustomer(principal.getUsername());
     }
+
+
 }
